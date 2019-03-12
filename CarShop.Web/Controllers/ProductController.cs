9053609c45dbe5cf2b01d7089fd26d868f151282
mehmet akif örs <mehmetakifors@hotmail.com>
2019-03-12 14:56:05 +0300

    Produc Kontroller Kısmen Yapıldı

---
+++ CarShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using CarShop.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarShop.Web.Controllers
+{
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
+        public ProductController(IProductService productService, ICategoryService categoryService) :base(categoryService)
+        {
+            this.productService = productService;
+            this.categoryService = categoryService;
+        }
+        // GET: Product
+        public ActionResult Index(Guid id)
+        {
+          ViewBag.Product = productService.Find(id); 
+            return View();
+        }
+    }
+}
