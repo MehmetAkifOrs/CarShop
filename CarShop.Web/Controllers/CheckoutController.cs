ff842ad027340f849d3b62363dbf23c800648526
mehmet akif örs <mehmetakifors@hotmail.com>
2019-03-14 14:50:48 +0300

    Card ve Checkout

--- CarShop.Web/Controllers/CheckoutController.cs
+++ CarShop.Web/Controllers/CheckoutController.cs
@@ -7,20 +7,18 @@ using System.Web.Mvc;
 
 namespace CarShop.Web.Controllers
 {
-   
-    public class CartController : ControllerBase
+    public class CheckoutController : ControllerBase
     {
         private readonly IProductService productService;
         private readonly ICategoryService categoryService;
-        public CartController(IProductService productService, ICategoryService categoryService) : base(categoryService)
+        public CheckoutController(IProductService productService, ICategoryService categoryService) : base(categoryService)
         {
             this.productService = productService;
             this.categoryService = categoryService;
         }
-        // GET: Cart
-        public ActionResult Index(Guid id)
+        // GET: Checkout
+        public ActionResult Index()
         {
-            ViewBag.Product = productService.Find(id);
             return View();
         }
     }
