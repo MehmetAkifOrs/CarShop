391eaec4ef8ff666c8de653c1bd842ce9c7f3cd8
mehmet akif örs <mehmetakifors@hotmail.com>
2019-03-26 12:25:24 +0300

    anasayfa foto

--- CarShop.Data/Migrations/201903260834202_Initial.Designer.cs
+++ CarShop.Data/Migrations/201903260834202_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace CarShop.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class InitialCreate : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201903252125471_InitialCreate"; }
+            get { return "201903260834202_Initial"; }
         }
         
         string IMigrationMetadata.Source
